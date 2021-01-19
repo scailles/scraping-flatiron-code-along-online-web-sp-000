f06258c7bb1f07178f3c94110b600dc351bb562b
Shaina C <scailles23@gmail.com>
2021-01-19 09:18:02 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -5,6 +5,19 @@ attr_accessor :title, :schedule, :description
 
 
 @@all= []
+
+
+  def initialize
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+ 
+  def self.reset_all
+    @@all.clear
+  end
   
   
 end
